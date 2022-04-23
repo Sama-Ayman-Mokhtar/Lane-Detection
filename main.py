f3ebbf1514c2e7c49e5205174bcb87099918c0a9
sama <sama2ayman2@gmail.com>
2022-04-23 21:50:13 +0200

    applyed sobel

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
 import cv2
+import glob
+import pickle
+import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("test_images/test1.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+myImage = cv2.imread("test_images/test1.jpg")
+myImage = cv2.cvtColor(myImage, cv2.COLOR_BGR2RGB)
 
+def edgeDetection_qThreshold(myImage, orientation='horizontal', kernelSize = 3, threshold = (0, 255)):
+    grayMyImage = cv2.cvtColor(myImage, cv2.COLOR_BGR2GRAY)
+
+    if orientation == 'horizontal':
+        sobelImage = cv2.Sobel(grayMyImage, cv2.CV_64F, 0, 1, kernelSize)
+    elif orientation == 'vertical':
+        sobelImage = cv2.Sobel(grayMyImage, cv2.CV_64F, 1, 0, kernelSize)
+
+    absSobelImage = np.abs(sobelImage)
+
+    scaledSobelImage = np.uint8(255*absSobelImage/np.max(absSobelImage))
+
+    thresholdSobelImage =  np.zeros_like(scaledSobelImage)
+    thresholdSobelImage[(scaledSobelImage >= threshold[0]) & (scaledSobelImage <= threshold[1])] = 1
+    
+    return thresholdSobelImage
+
+H_edgeDetectionImage = edgeDetection_qThreshold(myImage, 'horizontal', 3, (20,200))
+V_edgeDetectionImage = edgeDetection_qThreshold(myImage, 'vertical', 3, (20,200))
 
 fig = plt.figure(figsize=(8.5, 4.8))
 
 gs1 = plt.GridSpec(3, 3, left = 0.05, bottom = 0.05, right = 0.95, top = 0.95, wspace = 0.05, hspace = 0.05)
 ax1 = fig.add_subplot(gs1[:-1, :-1])
-ax1.imshow(img)
+ax1.imshow(myImage)
 ax1.axes.xaxis.set_visible(False)
 ax1.axes.yaxis.set_visible(False)
 
 ax2 = fig.add_subplot(gs1[-1, 0])
-ax2.imshow(img)
+ax2.imshow(H_edgeDetectionImage, cmap='gray')
 ax2.axes.xaxis.set_visible(False)
 ax2.axes.yaxis.set_visible(False)
 
 ax3 = fig.add_subplot(gs1[-1, 1])
-ax3.imshow(img)
+ax3.imshow(V_edgeDetectionImage, cmap='gray')
 ax3.axes.xaxis.set_visible(False)
 ax3.axes.yaxis.set_visible(False)
 
 ax4 = fig.add_subplot(gs1[-1, -1])
-ax4.imshow(img)
+ax4.imshow(myImage)
 ax4.axes.xaxis.set_visible(False)
 ax4.axes.yaxis.set_visible(False)
 
 ax5 = fig.add_subplot(gs1[1, -1])
-ax5.imshow(img)
+ax5.imshow(myImage)
 ax5.axes.xaxis.set_visible(False)
 ax5.axes.yaxis.set_visible(False)
 
 ax6 = fig.add_subplot(gs1[0, -1])
-ax6.imshow(img)
+ax6.imshow(myImage)
 ax6.axes.xaxis.set_visible(False)
 ax6.axes.yaxis.set_visible(False)
 plt.show()
